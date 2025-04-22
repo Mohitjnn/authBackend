1ddffec06696405da5dfa938f08e8b4f538fc0ac
Mohitjnn <jnmohit29@gmail.com>
2025-04-22 20:43:19 +0530

    update deployemeny

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ app = FastAPI()
 ALLOWED_ORIGIN = os.getenv("ALLOWED_URL")
 origins = [
     ALLOWED_ORIGIN,
+    "http://192.168.29.147:3000"
     "http://127.0.0.1:3000",
     "http://localhost:3000",
 ]
