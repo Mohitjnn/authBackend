f22dbe2be85962f866080cd536e4b9bbc50fcfc0
Mohitjnn <jnmohit29@gmail.com>
2025-04-22 21:02:11 +0530

    update deployement

--- config/config.py
+++ config/config.py
@@ -1,8 +1,11 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
+from dotenv import load_dotenv
 
-MONGO_URI = os.getenv("MONGO_URI")
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI","mongodb+srv://jnmohit29:Mohitjn123@cluster0.lfubgwo.mongodb.net/Cluster0?retryWrites=true&w=majority")
 uri = MONGO_URI
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi("1"))
